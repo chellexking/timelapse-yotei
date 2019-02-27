56f295d4817035dee7c28f8bdd928f0f4f2e243e
Michelle <chellezxxx@gmail.com>
2019-02-27 15:40:38 +0900

    merge timelapse and progress bar together into 1 file

--- pages/timeLapse.php
+++ pages/timeLapse.php
@@ -25,6 +25,44 @@
     min-height: 100%;
     position: absolute;
 }
+    
+    #myProgress {
+  width: 100%;
+  right: 0px;
+  background-color: dimgray;
+  opacity: .8;
+  bottom: 0px;
+  position: absolute;
+  z-index: 3;
+  left: 30px;
+  height: 27px;
+}
+
+#myBar {
+  margin-left: 25px; 
+  margin-top: 10px;
+  width: 1%;
+  height: 4px;
+  background-color: whitesmoke;
+}
+    
+#playPauseButton {
+    font-size: 15px;
+    background-color: dimgray;
+    position: absolute;
+    bottom: 0px;
+    z-index: 4;
+    padding: 5px 10px;
+    color: darkgray;
+    border-radius: 5px;
+}   
+
+    #playPauseButton:hover {
+        cursor: pointer;
+        background-color: darkgrey;
+        color: aliceblue;
+        border-radius: 5px;
+    }    
 </style>
 
 <div id="liveStream" style="display:none">   
@@ -36,13 +74,31 @@
         $image = '<img src="'.$num.'" style="max-height:100%; max-width:100%">';
       echo $image;
     }
-//    exit; 
     ?>
 
-<?php include 'progressBar.php' ?>
+    <div id="playPauseButton"> <i class="fa fa-play" aria-hidden="true"></i> </div>
+    <div id="myProgress">
+        <div id="myBar"></div>
+    </div>
     
 </div>
 
 <script>
         $("#liveStream > img:gt(0)").hide();
+    
+    function move() {
+  var elem = document.getElementById("myBar");  
+  var width = 1;
+  var id = setInterval(frame, 250);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+    
+    
 </script>
