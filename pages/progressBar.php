bdd1bc54b00bdd71c24673ce2637a251edb0b09e
Michelle <chellezxxx@gmail.com>
2019-02-27 15:37:47 +0900

    added play button to timelapse

--- pages/progressBar.php
+++ pages/progressBar.php
@@ -3,21 +3,46 @@
 <style>
 #myProgress {
   width: 100%;
+  right: 0px;
   background-color: dimgray;
   opacity: .8;
   bottom: 0px;
   position: absolute;
   z-index: 3;
+  left: 30px;
+  height: 27px;
 }
 
 #myBar {
+  margin-left: 25px; 
+  margin-top: 10px;
   width: 1%;
-  height: 13px;
+  height: 4px;
   background-color: whitesmoke;
 }
     
+#playPauseButton {
+    font-size: 15px;
+    background-color: dimgray;
+    position: absolute;
+    bottom: 0px;
+    z-index: 4;
+    padding: 5px 10px;
+    color: darkgray;
+    border-radius: 5px;
+}   
+
+    #playPauseButton:hover {
+        cursor: pointer;
+        background-color: darkgrey;
+        color: aliceblue;
+        border-radius: 5px;
+    }    
+    
 </style>
 <body>
+
+    <div id="playPauseButton"> <i class="fa fa-play" aria-hidden="true"></i> </div>
     <div id="myProgress">
         <div id="myBar"></div>
     </div>
@@ -38,6 +63,8 @@ function move() {
     }
   }
 }
+    
+    
 </script>
 
 </html>
--- pages/timeLapse.php
+++ pages/timeLapse.php
@@ -1,3 +1,32 @@
+<!DOCTYPE html> 
+<style>
+/*timelapse video */
+#liveStream {
+    max-height: 100%;
+    max-width: 100%;
+    min-width: 100%;
+    background-position: center;
+    background-size: cover;
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 80px;
+    overflow: hidden;
+}
+
+#liveStream > img { 
+    object-position: center;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    max-width: 100%;
+    min-width: 1024px;
+    min-height: 100%;
+    position: absolute;
+}
+</style>
+
 <div id="liveStream" style="display:none">   
     
     <?php
